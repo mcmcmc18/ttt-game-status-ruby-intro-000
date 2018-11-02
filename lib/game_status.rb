1ee793c62812237b3d8c45af8f4edc848ad9a414
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-02 21:35:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,11 +17,11 @@ WIN_COMBINATIONS = [
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
-  win_index_1 = win_combination[3]
-  win_index_2 = win_combination[4]
-  win_index_3 = win_combination[5]
-  win_index_1 = win_combination[6]
-  win_index_2 = win_combination[7]
+  win_index_4 = win_combination[3]
+  win_index_5 = win_combination[4]
+  win_index_6 = win_combination[5]
+  win_index_7 = win_combination[6]
+  win_index_8 = win_combination[7]
   
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
@@ -31,7 +31,11 @@ WIN_COMBINATIONS = [
   position_6 = board[win_index_6]
   position_7 = board[win_index_7]
   position_8 = board[win_index_8]
+  position_9 = board[win_index_8]
   
   def won?(board)
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
+   return win_combination
+    else if position_2 == "X" && position_3 == "X" && position_3 == "X"
+    else if position_1 == "X" && position_2 == "X" && position_3 == "X"
         
