e6a93809abae30d4156b0574ed32cfa856689a50
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-02 21:52:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,9 +22,9 @@ WIN_COMBINATIONS = [
   win_index_2 = win_combination[2]
   
   
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
+  position_1 = board[win_index_0]
+  position_2 = board[win_index_1]
+  position_3 = board[win_index_2]
   
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
    return win_combination
