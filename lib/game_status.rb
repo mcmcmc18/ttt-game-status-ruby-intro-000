5865e1bd2b23de71201abb9235a915300e43048e
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-02 21:50:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,4 +57,11 @@ def winner?(board)
   index = won?(board)
   if index == false
     return nil
-    
+      else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
