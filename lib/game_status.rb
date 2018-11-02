db0e4ead55b40dcc00b4b5dded056bbf68b0a781
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-02 21:50:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [0,4,8]
   ]
   
-    def won?(board)
+  def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
